b20cbc5f19312232d434f9177b0965e4b77e1deb
Thorsten Henke <mail@thorsten-henke.de>
2018-08-04 21:09:40 +0200

    Full set of examples (network, irgaph, list, ...)

--- example.R
+++ example.R
@@ -1,12 +1,18 @@
-library(statnet)
+library(network)
+library(igraph)
 
-data("faux.magnolia.high")
+data("faux.magnolia.high", package = "ergm")
 fmh <- faux.magnolia.high ; rm(faux.magnolia.high)
 
 x <- as.sociomatrix(fmh)
 v <- get.vertex.attribute(fmh, "Grade")
 
-soc_closing(fmh, "Grade")
-soc_closing(x, v)
+gg <- graph_from_adjacency_matrix(x)
+vertex_attr(gg, "Grade") <- v
 
-# ??? Igraph??? => List?
+social_closing(fmh, "Grade")
+social_closing(gg, "Grade")
+social_closing(list(fmh, fmh), "Grade")
+social_closing(list(gg, fmh), "Grade")
+
+social_closing(x, v)
