d306d76034cfc0ab54ea00b4825095fa7efa465c
Thorsten Henke <mail@thorsten-henke.de>
2018-10-09 11:09:07 +0200

    Collection of ideas for new indices within the package.

--- example.R
+++ example.R
@@ -1,14 +1,12 @@
-library(network)
-library(igraph)
 
 data("faux.magnolia.high", package = "ergm")
 fmh <- faux.magnolia.high ; rm(faux.magnolia.high)
 
-x <- as.sociomatrix(fmh)
-v <- get.vertex.attribute(fmh, "Grade")
+x <- network::as.sociomatrix(fmh)
+v <- network::get.vertex.attribute(fmh, "Grade")
 
-gg <- graph_from_adjacency_matrix(x)
-vertex_attr(gg, "Grade") <- v
+gg <- igraph::graph_from_adjacency_matrix(x)
+igraph::vertex_attr(gg, "Grade") <- v
 
 social_closing(fmh, "Grade")
 social_closing(gg, "Grade")
