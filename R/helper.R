e923507eac8104f241a230af5d1c8e91650af387
Thorsten Henke <mail@thorsten-henke.de>
2018-10-09 11:10:33 +0200

    Reduce code reduncency by use of internal methods

--- R/helper.R
+++ R/helper.R
@@ -1,5 +1,6 @@
 is_sociomatrix <- function(x) {
-    is.matrix(x) && isSymmetric(x) && all((x == 0) | (x == 1)) && (all(diag(x) == 0))
+    #is.matrix(x) && isSymmetric(x) && all((x == 0) | (x == 1)) && (all(diag(x) == 0))
+    is.matrix(x) && all((x == 0) | (x == 1)) && (all(diag(x) == 0))
 }
 
 is_nominal <- function(v) {
@@ -8,6 +9,22 @@ is_nominal <- function(v) {
 
 is_integer <- function(v) {
     is.integer(v) || all(v %% 1 == 0)
+}
+
+apply_list <- function(fun, net, vname, depth = 0) {
+    if (!(is.character(vname) && length(vname) == 1)) {
+        stop("Fehler")
+    }
+
+    depth <- depth + 1
+
+    n <- length(net)
+    al <- vector(mode = "list", length = n)
+    for (i in 1:n) {
+        al[[i]] <- fun(net[[i]], vname, depth = depth)
+    }
+
+    compose(al)
 }
 
 compose <- function(xl) {
@@ -29,4 +46,9 @@ compose <- function(xl) {
     }
 
     res
+}
+
+print_err_msg <- function(fun_name, x) {
+    cc <- paste(class(x), collapse = " ")
+    stop(paste0("Method ", fun_name, " is not applicable to data of class ", cc))
 }
