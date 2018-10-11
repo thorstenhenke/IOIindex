10f08190d7242c6a78390d0bca96884df05ccd11
Thorsten Henke <mail@thorsten-henke.de>
2018-10-11 08:48:00 +0200

    Implemented the indices from the Reitz paper

--- R/internal.R
+++ R/internal.R
@@ -13,3 +13,33 @@ Aijk_Rintern <- function(x, v) {
 Ii_Rintern <- function(x, v) {
     Aijk_Rintern(x, v) - Eijk_Rintern(x, v)
 }
+
+
+EReitz_intern <- function(x, v, logscale = FALSE) {
+    xg <- outer(v, v, "==")
+    re <- rowSums(x * xg)
+    rn <- rowSums(xg)
+    if (logscale) {
+        e <- log(re + 1) - log(rn)
+    } else {
+        e <- re/(rn - 1)
+    }
+    e
+}
+
+AReitz_intern <- function(x, v) {
+    xg <- outer(v, v, "=!")
+    re <- rowSums(x * xg)
+    rn <- rowSums(xg)
+    if (logscale) {
+        a <- log(re + 1) - log(rn + 1)
+    } else {
+        a <- re/rn
+    }
+    a
+}
+
+# Idea
+wa <- rowSums(m * outer(v,v,"=="))
+we <- rowSums(m * outer(v,v,"!="))
+log(wa + 1) - log(we + 1)
