b898b9ea0920b12147a7f160f1df56c61a11bdc5
Thorsten Henke <thorsten.henke@iew.uni-hannover.de>
2019-05-02 08:29:00 +0200

    Added and extended the function documentation.

--- R/caller.R
+++ R/caller.R
@@ -186,48 +186,73 @@ social_integration.default <- function(x, depth = 0, ...) {
     print_err_msg("social_integration()", x)
 }
 
-
+#' Naive in- and outgroup index
+#'
+#' This index was described and used in various papers (see e.g. Reitz, Asendorpf & Motti-Stefanidi, 2015, DOI: 10.1177/0165025414567008) For this index a ratio
+#' between the actual number relationships to members of the in- or outgroup is compared to
+#' the maximum possible number of connections within the in- or outgroup
+#' (aka the size of the in- or outgroup).
+#'
+#' @inheritParams social_closing
 naive_opening <- function(net, ...) {
     UseMethod("naive_opening")
 }
 
+#' @describeIn naive_opening net is of type list
 naive_opening.list <- function(net, vname, depth = 0) {
     apply_list(naive_opening, net, vname, depth)
 }
+#' @describeIn naive_opening net is of type network
 naive_opening.network <- function(net, vname) {
     obj <- extractor_network(net, vname)
     AReitz_intern(obj$x, obj$v)
 }
+#' @describeIn naive_opening net is of type igraph
 naive_opening.igraph <- function(net, vname) {
     obj <- extractor_igraph(net, vname)
     AReitz_intern(obj$x, obj$v)
 }
+#' @describeIn naive_opening net is of type matrix
 naive_opening.matrix <- function(net, v) {
     AReitz_intern(net, v)
 }
+#' @describeIn naive_opening error case
 naive_opening.default <- function(x, depth = 0, ...) {
     print_err_msg("naive_opening()", x)
 }
 
-
+#' Naive in- and outgroup index
+#'
+#' This index was described and used in various papers (see e.g. Reitz, Asendorpf & Motti-Stefanidi, 2015, DOI: 10.1177/0165025414567008) For this index a ratio
+#' between the actual number relationships to members of the in- or outgroup is compared to
+#' the maximum possible number of connections within the in- or outgroup
+#' (aka the size of the in- or outgroup).
+#'
+#' @describeIn naive_opening general implementation for S3 class dispatch
 naive_closing <- function(net, ...) {
     UseMethod("naive_closing")
 }
 
+#' @describeIn naive_opening net is of type list
 naive_closing.list <- function(net, vname, depth = 0) {
     apply_list(naive_closing, net, vname, depth)
 }
+
+#' @describeIn naive_opening net is of type network
 naive_closing.network <- function(net, vname) {
     obj <- extractor_network(net, vname)
     EReitz_intern(obj$x, obj$v)
 }
+#' @describeIn naive_opening net is of type igraph
 naive_closing.igraph <- function(net, vname) {
     obj <- extractor_igraph(net, vname)
     EReitz_intern(obj$x, obj$v)
 }
+#' @describeIn naive_opening net is of type matrix
 naive_closing.matrix <- function(net, v) {
     EReitz_intern(net, v)
 }
+#' @describeIn naive_opening error case
 naive_closing.default <- function(net, ...) {
     print_err_msg("naive_closing()", net)
 }
@@ -254,12 +279,20 @@ naive_balance.default <- function(x, depth = 0, ...) {
     print_err_msg("naive_balance()", x)
 }
 
+
+#' Social homophily Index
+#'
+#' `McCormick()` calculates a homophily index based on a given network and node attribute.
+#' This index was invented by McCormick, Cappella, Hughes, Gallagher (2015). DOI: 10.1177/0272431614547051. During the developement of the IOIndex this index was used for validation.
+#' Thus both indices, the IOIndex and McCormick's index, should usually be moderatedly correlated.
+#'
 #' @inheritParams social_closing
 #' @return a numeric vector of social opening indices for each indvidual in the network.
 McCormick <- function(net, ..., depth = 0) {
     UseMethod("McCormick")
 }
 
+#' @describeIn McCormick net is of type list
 McCormick.list <- function(net, vname, ..., depth = 0) {
     if (is.matrix(net[[1]])) {
         mi <- apply_list_matrix(McCormick, net, vname, depth)
@@ -269,20 +302,24 @@ McCormick.list <- function(net, vname, ..., depth = 0) {
     mi
 }
 
+#' @describeIn McCormick net is of type network
 McCormick.network <- function(net, vname, ...) {
     obj <- extractor_network(net, vname)
     McCormick_intern(obj$x, obj$v, ...)
 }
 
+#' @describeIn McCormick net is of type igraph
 McCormick.igraph <- function(net, vname, ...) {
     obj <- extractor_igraph(net, vname)
     McCormick_intern(obj$x, obj$v, ...)
 }
 
+#' @describeIn McCormick net is of type network
 McCormick.matrix <- function(net, v, ...) {
     McCormick_intern(net, v, ...)
 }
 
+#' @describeIn McCormick error case
 McCormick.default <- function(x, ...) {
     print_err_msg("McCormick()", x)
 }
